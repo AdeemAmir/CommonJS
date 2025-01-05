27492da894c6e4618987d1a42c0e7c9cd3ab291d
AdeemAmir <adeemamir21@gmail.com>
2025-01-06 00:21:27 +0500

    Reworked
    raw no longer needed.

--- manage.mjs
+++ manage.mjs
@@ -1,23 +1,26 @@
-function ciciphus(text, shift) {
-const alphU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const alphL = 'abcdefghijklmnopqrstuvwxyz';
-const nmn = '0123456789';
-let fallback = '';
-for (let i = 0; i < text.length; i++) {let a = text[i];
-if (alphL.includes(a)) {
-const lf = (alphL.indexOf(a) + shift + 26) % 26; 
-fallback += alphL[lf];
-}else if (alphU.includes(a)) {
-const lf = (alphU.indexOf(a) + shift + 26) % 26;
-fallback += alphU[lf];
-}else if (nmn.includes(a)) {
-const lf = (nmn.indexOf(a) + shift + 10) % 10; 
-fallback += nmn[lf];
-}else {fallback += a;}}return fallback;}
-export async function xpCol() {try {
-        const rez = await fetch('CommonJS/raw');
-        const ciphX = await rez.text();
-        const deciF = ciciphus(ciphX, -3);
-        return deciF;} catch (error) {
-        console.error('Error f:d: ', error);
-return null;}}
+const loader = 'https://raw.githubusercontent.com/AdeemAmir/Private/refs/heads/main/raw?token=GHSAT0AAAAAAC4M2KRQQGB3N7G23OELPPPOZ324MBA';
+function alphl(n,b){
+const alphU='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphL='abcdefghijklmnopqrstuvwxyz';
+const nmn='0123456789';
+let fb='';
+for (let i=0;i<n.length;i++){let a=n[i];
+if (alphL.includes(a)){
+const lf=(alphL.indexOf(a)+b+26)%26; 
+fb+=alphL[lf];
+}else if(alphU.includes(a)){
+const lf=(alphU.indexOf(a)+b+26)%26;
+fb+=alphU[lf];
+}else if (nmn.includes(a)){
+const lf=(nmn.indexOf(a)+b+10)%10; 
+fb+=nmn[lf];
+}else{fb += a;}}
+return fb;}export async function xpCol(y,l)
+{function ciphx() {return deciF}
+const rez = await fetch(loader);
+const ciphX = await rez.text();
+const lN = ciphX.split('\n');
+const sL = lN[y-1];
+if(l===1)var deciF = alphl(sL,0);
+else if(l==="1")var deciF = alphl(sL,+3)
+else var deciF = alphl(sL,-3);return ciphx();}
