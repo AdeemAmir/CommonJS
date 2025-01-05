8f2a7d9028c72e1dd0f998f365be420a26ff54f4
AdeemAmir <adeemamir21@gmail.com>
2025-01-06 00:44:37 +0500

    added cBasics.dll

--- manage.mjs
+++ manage.mjs
@@ -1,4 +1,3 @@
-const loader = 'https://raw.githubusercontent.com/AdeemAmir/Private/refs/heads/main/raw?token=GHSAT0AAAAAAC4M2KRQQGB3N7G23OELPPPOZ324MBA';
 function alphl(n,b){
 const alphU='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const alphL='abcdefghijklmnopqrstuvwxyz';
@@ -17,8 +16,8 @@ fb+=nmn[lf];
 }else{fb += a;}}
 return fb;}export async function xpCol(y,l)
 {function ciphx() {return deciF}
-const rez = await fetch(loader);
-const ciphX = await rez.text();
+const rez = await fetch("cBasics.dll");
+const ciphX = (await rez.text()).replace(/\r/g,'');
 const lN = ciphX.split('\n');
 const sL = lN[y-1];
 if(l===1)var deciF = alphl(sL,0);
